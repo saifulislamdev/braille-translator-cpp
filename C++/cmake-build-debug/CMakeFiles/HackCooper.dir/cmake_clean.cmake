6028253725c14788ad739ac4db883d5657cbeaa9
Saiful Islam <nysaifulislam@gmail.com>
2019-09-17 17:29:59 -0400

    initial commit

---
+++ C++/cmake-build-debug/CMakeFiles/HackCooper.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HackCooper.dir/main.cpp.o"
+  "HackCooper.pdb"
+  "HackCooper"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HackCooper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C++/main.cpp
@@ -0,0 +1,232 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string output;
+    char character;
+    while (cin >> character) {
+        switch (character) {
+            // Spaces
+            case ' ':
+                output += "          ";
+                break;
+            case '\t':
+                output += "\t";
+                break;
+            case '\n':
+                output += "\n";
+                break;
+            case '\v':
+                output += "\v";
+                break;
+            case '\f':
+                output += "\f";
+                break;
+            case '\r':
+                output += "\r";
+                break;
+
+                // Letters
+            case 'a':
+            case 'A': {
+                output += "⠁";
+                break;
+            }
+            case 'b':
+            case 'B': {
+                output += "⠃";
+                break;
+            }
+            case 'c':
+            case 'C': {
+                output += "⠉";
+                break;
+            }
+            case 'd':
+            case 'D': {
+                output += "⠙";
+                break;
+            }
+            case 'e':
+            case 'E': {
+                output += "⠑";
+                break;
+            }
+            case 'f':
+            case 'F': {
+                output += "⠋";
+                break;
+            }
+            case 'g':
+            case 'G': {
+                output += "⠛";
+                break;
+            }
+            case 'h':
+            case 'H': {
+                output += "⠓";
+                break;
+            }
+            case 'i':
+            case 'I': {
+                output += "⠊";
+                break;
+            }
+            case 'j':
+            case 'J': {
+                output += "⠚";
+                break;
+            }
+            case 'k':
+            case 'K': {
+                output += "⠅";
+                break;
+            }
+            case 'l':
+            case 'L': {
+                output += "⠇";
+                break;
+            }
+            case 'm':
+            case 'M': {
+                output += "⠍";
+                break;
+            }
+            case 'n':
+            case 'N': {
+                output += "⠝";
+                break;
+            }
+            case 'o':
+            case 'O': {
+                output += "⠕";
+                break;
+            }
+            case 'p':
+            case 'P': {
+                output += "⠏";
+                break;
+            }
+            case 'q':
+            case 'Q': {
+                output += "⠟";
+                break;
+            }
+            case 'r':
+            case 'R': {
+                output += "⠗";
+                break;
+            }
+            case 's':
+            case 'S': {
+                output += "⠎";
+                break;
+            }
+            case 't':
+            case 'T': {
+                output += "⠞";
+                break;
+            }
+            case 'u':
+            case 'U': {
+                output += "⠥";
+                break;
+            }
+            case 'v':
+            case 'V': {
+                output += "⠧";
+                break;
+            }
+            case 'w':
+            case 'W': {
+                output += "⠺";
+                break;
+            }
+            case 'x':
+            case 'X': {
+                output += "⠭";
+                break;
+            }
+            case 'y':
+            case 'Y': {
+                output += "⠽";
+                break;
+            }
+            case 'z':
+            case 'Z': {
+                output += "⠵";
+                break;
+            }
+
+                // Numbers
+            case '0':
+                output += "⠼⠚";
+                break;
+            case '1':
+                output += "⠼⠁";
+                break;
+            case '2':
+                output += "⠼⠃";
+                break;
+            case '3':
+                output += "⠼⠉";
+                break;
+            case '4':
+                output += "⠼⠙";
+                break;
+            case '5':
+                output += "⠼⠑";
+                break;
+            case '6':
+                output += "⠼⠋";
+                break;
+            case '7':
+                output += "⠼⠛";
+                break;
+            case '8':
+                output += "⠼⠓";
+                break;
+            case '9':
+                output += "⠼⠊";
+                break;
+
+                // Symbols
+            case '.':
+                output += "⠲";
+                break;
+            case '-':
+                output += "⠤";
+                break;
+            case ';':
+                output += "⠆";
+                break;
+            case '\"':
+                output += "⠄⠶";
+                break;
+            case '!':
+                output += "⠖";
+                break;
+            case '(':
+                output += "⠐⠣";
+                break;
+            case ')':
+                output += "⠐⠜";
+                break;
+            case ',':
+                output += "⠂";
+                break;
+            case '@':
+                output += "⠜";
+                break;
+            case '*':
+                output += "⠔";
+                break;
+
+            default:
+                output += "This character is not yet registered on the system";
+        }
+    }
+cout << output << endl;
+    return EXIT_SUCCESS;
+}
